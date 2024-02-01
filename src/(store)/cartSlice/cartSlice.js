37e9f6114cc1cbf4293bdd696b7ce91305f0dde0
OXAYR <130404981+OXAYR@users.noreply.github.com>
2024-02-01 17:46:17 +0500

    Cart slice has been made

--- src/(store)/cartSlice/cartSlice.js
+++ src/(store)/cartSlice/cartSlice.js
@@ -1,6 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState {
+const initialState = {
     lineItems: [],
 };
 
