3ab657c8998b82ef90eb7b48e4ec487fe2056f7c
OXAYR <130404981+OXAYR@users.noreply.github.com>
2024-02-12 13:22:55 +0500

    Added loader on the persist state

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,12 @@ import { Provider } from 'react-redux';
 import { store, persistor } from '../(store)/store';
 import Navbar from '@/components/Layout/Navbar';
 import { PersistGate } from 'redux-persist/integration/react'
+import Loader from '@/components/loader';
 
 function MyApp({ Component, pageProps }) {
     return (
         <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
+            <PersistGate loading={<Loader />} persistor={persistor}>
                 <div className='bg-gray-50'>
                     <Navbar />
                     <Component {...pageProps} />
